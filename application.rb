8baec82cc30534611f61598d69d075608c38d6e3
Kevin McPhillips <github@kevinmcphillips.ca>
2010-08-01 14:50:54 -0500

    Got rot13() working. Updated bio and resume content. Working on nav CSS.

--- application.rb
+++ application.rb
@@ -37,7 +37,7 @@ helpers do
   def rot13email(email, name=nil)
     obfuscated = email.clone
     obfuscated.insert((email.length / 3) * 2, "[REMOVETHIS]").insert(email.length / 3, "[REMOVETHIS]")
-    "<script>document.write('<a href=\"mailto:' + '#{email.rot13}'.rot13 + '\">#{name || "' + '#{email.rot13}'.rot13 + '"}</a>');</script><noscript><a href=\"mailto:#{obfuscated}\">#{name || obfuscated}</a></noscript>"
+    "<script>document.write('<a href=\"mailto:' + '#{email.rot13}'.rot13() + '\">#{name || "' + '#{email.rot13}'.rot13() + '"}</a>');</script><noscript><a href=\"mailto:#{obfuscated}\">#{name || obfuscated}</a></noscript>"
   end
 end
 
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,4 +2,9 @@ $(document).ready(function($) {
    
 });
 
-// TODO implement rot13
+String.prototype.rot13 = function(){
+	return this.replace(/[a-zA-Z]/g, function(c){
+		return String.fromCharCode((c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26);
+	});
+};
+
