6a315ce029fa82709d0cbee64c3deb6081c92de1
Kevin McPhillips <kevin.mcphillips@canadadrugs.com>
2011-02-18 15:28:01 -0600

    Add the working RSS feed finally

--- app/app.rb
+++ app/app.rb
@@ -3,28 +3,6 @@ class KevinmcphillipsCa < Padrino::Application
   register Padrino::Mailer
   register Padrino::Helpers
 
-  ##
-  # Application configuration options
-  #
-  # set :raise_errors, true     # Show exceptions (default for development)
-  # set :public, "foo/bar"      # Location for static assets (default root/public)
-  # set :reload, false          # Reload application files (default in development)
-  # set :default_builder, "foo" # Set a custom form builder (default 'StandardFormBuilder')
-  # set :locale_path, "bar"     # Set path for I18n translations (defaults to app/locale/)
-  # enable  :sessions           # Disabled by default
-  # disable :flash              # Disables rack-flash (enabled by default if sessions)
-  # layout  :my_layout          # Layout can be in views/layouts/foo.ext or views/foo.ext (default :application)
-  #
-
-  ##
-  # You can configure for a specified environment like:
-  #
-  #   configure :development do
-  #     set :foo, :bar
-  #     disable :asset_stamp # no asset timestamping for dev
-  #   end
-  #
-
   error 404 do
     haml :'404'
   end
@@ -38,6 +16,11 @@ class KevinmcphillipsCa < Padrino::Application
     @posts = Post.page pager_params(:page => params[:page], :order => [:created_at.desc]) 
     haml :'blog'
   end
+  
+  get 'feed', :provides => [:rss] do
+    @posts = Post.all :order => [:created_at.desc]
+    render :feed
+  end
 
   get '/blog/:permalink' do
     @post = Post.first :permalink => params[:permalink]
@@ -47,10 +30,6 @@ class KevinmcphillipsCa < Padrino::Application
     else
       haml :'404'
     end
-  end
-
-  get "/blog.rss" do
-    builder :blog
   end
 
   ## All other sections are pretty simple
