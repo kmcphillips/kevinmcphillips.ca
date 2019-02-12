c6101c21531a2a440273917559095b75154b1334
Kevin McPhillips <github@kevinmcphillips.ca>
2019-02-11 19:09:33 -0500

    Serve keybase.txt from nginx

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,16 +8,9 @@ role :db, "app1.kev.cool"
 server "app1.kev.cool", user: "deploy", roles: %w{app db web}
 
 after "deploy:publishing", "deploy:restart"
-after "deploy:publishing", "deploy:symlink_shared_files"
 
 namespace :deploy do
   task :restart do
     invoke "unicorn:legacy_restart"
-  end
-
-  task :symlink_shared_files do
-    on roles(:app) do
-      execute "ln -s #{ shared_path }/keybase.txt #{ release_path }/public/keybase.txt"
-    end
   end
 end
