7c1cf51ed35564a24d53ec32b7f72f1771c0adc9
Kevin McPhillips <github@kevinmcphillips.ca>
2019-06-21 15:05:35 -0400

    Missing one reference to unicorn

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,9 +8,3 @@ role :db, "app1.kev.cool"
 server "app1.kev.cool", user: "deploy", roles: %w{app db web}
 
 after "deploy:publishing", "deploy:restart"
-
-namespace :deploy do
-  task :restart do
-    invoke "unicorn:legacy_restart"
-  end
-end
